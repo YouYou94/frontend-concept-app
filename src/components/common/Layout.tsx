42146ec3e730011173f675174f43e0b09a06999c
YouYou94 <ybh4115@gmail.com>
2023-07-27 20:32:37 +0900

    init: Layout

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+/* 기본 크기는 414px */
+export const Layout = styled.section`
+  width: 25.875rem;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  padding: 0.25rem;
+`;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 /* Common Components */
+export { Layout } from "./common/Layout";
+
 export { CommonButton } from "./common/Button";
 export { SmallButton } from "./common/Button";
