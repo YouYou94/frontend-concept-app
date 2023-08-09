d5bb2377d8006eec55e9031b50048c18bf76bf84
YouYou94 <ybh4115@gmail.com>
2023-08-09 23:03:07 +0900

    init: Title Common Component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,6 +4,9 @@ export { Layout } from "./common/Layout";
 export { CommonButton } from "./common/Button";
 export { SmallButton } from "./common/Button";
 
+export { Title } from "./common/Title";
+export { SubTitle } from "./common/Title";
+
 /* Components */
 export { HomeTitle } from "./HomeTitle";
 export { HomeMenu } from "./HomeMenu";
