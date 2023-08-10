8227ccba1a8206ac1d7e40a64b4d39d57218e2b9
YouYou94 <ybh4115@gmail.com>
2023-08-10 20:44:55 +0900

    Design: Layout Component

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
-/* 기본 크기는 414px */
-export const Layout = styled.section`
+/* 기본 크기는 100% */
+/* 모바일 화면에 맞춰 최소 414px까지 */
+export const Layout = styled.main`
   width: 100%;
   height: 100vh;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
