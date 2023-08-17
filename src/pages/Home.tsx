feb4765798ae64241495e4845a468bf76dddb661
YouYou94 <ybh4115@gmail.com>
2023-08-17 20:54:36 +0900

    Import: Menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Layout, TopBar } from "../components";
+import { Layout, TopBar, Menu } from "../components";
 
 const Home = () => {
   return (
     <Layout>
       <TopBar />
+      <Menu />
     </Layout>
   );
 };
