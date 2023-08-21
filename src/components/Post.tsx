f28b5b10e12a5fffc601c2595738a4e07bb5ad76
YouYou94 <ybh4115@gmail.com>
2023-08-21 21:42:54 +0900

    Design: Post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,11 @@
+import styled from "styled-components";
+
 export const Post = () => {
-  return <></>;
+  return <Box></Box>;
 };
+
+const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
