16eab1e8d0b202f49e29102438214b4ad5933b77
YouYou94 <ybh4115@gmail.com>
2023-08-23 18:35:48 +0900

    Design: Post Component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,11 +1,36 @@
 import styled from "styled-components";
 
 export const Post = () => {
-  return <Box></Box>;
+  return (
+    <Layout>
+      <Box>
+        <PostNumber></PostNumber>
+      </Box>
+      <Box>
+        <PostTitle></PostTitle>
+      </Box>
+      <Box>
+        <PostContent></PostContent>
+      </Box>
+      <Box>
+        <PostSettingButton></PostSettingButton>
+      </Box>
+    </Layout>
+  );
 };
 
-const Box = styled.div`
+const Layout = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
+
+const Box = styled.div``;
+
+const PostNumber = styled.span``;
+
+const PostTitle = styled.h3``;
+
+const PostContent = styled.span``;
+
+const PostSettingButton = styled.button``;
