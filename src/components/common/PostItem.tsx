bc921b2674e5fca898f5b712cfbef286d1a2f1e6
YouYou94 <ybh4115@gmail.com>
2023-08-25 22:43:24 +0900

    Refactor: 파일 이름 변경

---
+++ src/components/common/PostItem.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+type PostProps = {
+  postNumber: number;
+  postTitle: string;
+  postContent: string;
+};
+
+export const PostItem = ({ postNumber, postTitle, postContent }: PostProps) => {
+  return (
+    <Layout>
+      <Box>
+        <PostNumber>{postNumber}</PostNumber>
+      </Box>
+      <Box>
+        <PostTitle>{postTitle}</PostTitle>
+      </Box>
+      <Box>
+        <PostContent>{postContent}</PostContent>
+      </Box>
+      <Box>
+        <PostSettingButton></PostSettingButton>
+      </Box>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Box = styled.div``;
+
+const PostNumber = styled.span``;
+
+const PostTitle = styled.h3``;
+
+const PostContent = styled.span``;
+
+const PostSettingButton = styled.button``;
