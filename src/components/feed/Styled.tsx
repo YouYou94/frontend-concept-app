9e7b12d7bdedc7cf10e53a25f061dff71309aeb9
YouYou94 <ybh4115@gmail.com>
2023-08-28 20:12:34 +0900

    Design: Feed Component

--- src/components/feed/Styled.tsx
+++ src/components/feed/Styled.tsx
@@ -1,1 +1,24 @@
 import styled from "styled-components";
+
+export const Layout = styled.div`
+  height: 2rem;
+
+  display: flex;
+
+  padding: 0 3rem;
+`;
+
+export const Box = styled.div<{
+  width?: number;
+  height?: number;
+  flex?: number;
+}>`
+  flex: ${(prop) => prop.flex};
+
+  width: ${(prop) => prop.width}rem;
+  height: ${(prop) => prop.height}rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
