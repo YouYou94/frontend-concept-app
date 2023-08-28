95d13a51dfa5b9413488e24e5ed03e4b3b6ab4ba
YouYou94 <ybh4115@gmail.com>
2023-08-28 20:12:54 +0900

    Add: Feed Component Styled

--- src/components/feed/index.tsx
+++ src/components/feed/index.tsx
@@ -1,3 +1,17 @@
-export const Feed = () => {
-  return <></>;
+import { Box, Layout } from "./Styled";
+
+interface Props {
+  feedNumber: number;
+  feedTitle: string;
+  feedContent: string;
+}
+
+export const Feed = ({ feedNumber, feedTitle, feedContent }: Props) => {
+  return (
+    <Layout>
+      <Box width={2} height={2}></Box>
+      <Box width={10} height={2}></Box>
+      <Box flex={1}></Box>
+    </Layout>
+  );
 };
